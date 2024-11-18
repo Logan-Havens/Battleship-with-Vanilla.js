888192da0bfbe6fd28bf81561bc65dd52581b8d4
Logan Havens <153969446+Logan-Havens@users.noreply.github.com>
2024-11-18 19:42:38 +0000

    kinda got it working

--- src/app.js
+++ src/app.js
@@ -6,6 +6,74 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let gameBoard = [
+    [1, 1, 1, 1, 1, 0, 0, 0, 0, 1],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [1, 0, 0, 1, 1, 0, 0, 0, 0, 0],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [1, 1, 1, 1, 0, 0, 0, 0, 0, 0]
+  ];
+
+  function renderBoard() {
+    const container = document.getElementById("game-container");
+    container.innerHTML = "";
+
+    gameBoard.forEach((row, rowIndex) => {
+      row.forEach((cell, colIndex) => {
+        const cellDiv = document.createElement("div");
+        cellDiv.dataset.row = rowIndex;
+        cellDiv.dataset.col = colIndex;
+        cellDiv.classList.add("cell");
+
+        if (cell === 2) cellDiv.classList.add("sunken");
+        if (cell === 3) cellDiv.classList.add("missed");
+
+        cellDiv.addEventListener("click", () =>
+          fireTorpedo(rowIndex, colIndex)
+        );
+        container.appendChild(cellDiv);
+      });
+    });
+  }
+
+  function fireTorpedo(row, col) {
+    if (gameBoard[row][col] === 1) {
+      alert("Hit!");
+      gameBoard[row][col] = 2;
+    } else if (gameBoard[row][col] === 0) {
+      alert("Miss!");
+      gameBoard[row][col] = 3;
+    } else {
+      alert("Already fired here!");
+    }
+    renderBoard();
+  }
+
+  function showShips() {
+    const cells = document.querySelectorAll("#game-container div");
+    gameBoard.forEach((row, rowIndex) => {
+      row.forEach((cell, colIndex) => {
+        if (cell === 1) {
+          const cellDiv = Array.from(cells).find(
+            div => div.dataset.row == rowIndex && div.dataset.col == colIndex
+          );
+          if (cellDiv) {
+            cellDiv.style.backgroundColor = "yellow";
+          }
+        }
+      });
+    });
+  }
+
+  document.body.innerHTML +=
+    '<div id="game-container" class="game-container"></div>';
+  document.body.innerHTML += '<button id="show-ships">Show Ships</button>';
+
+  document.getElementById("show-ships").addEventListener("click", showShips);
+  renderBoard();
 };
